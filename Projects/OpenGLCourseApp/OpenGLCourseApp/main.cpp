e5e2aa3fcb514de8a1ab522c52697af6489af7e8
EricBL3 <ericbuitron3@gmail.com>
2023-11-02 19:16:59 -0600

    Add GLM and apply transformations to triangle

--- Projects/OpenGLCourseApp/OpenGLCourseApp/main.cpp
+++ Projects/OpenGLCourseApp/OpenGLCourseApp/main.cpp
@@ -1,12 +1,31 @@
 #include <stdio.h>
 #include <string.h>
+#include <cmath>
+
 #include <GL/glew.h>
 #include<GLFW/glfw3.h>
 
+#include<glm/glm.hpp>
+#include<glm/gtc/matrix_transform.hpp>
+#include<glm/gtc/type_ptr.hpp>
+
 // Window dimensions
 const GLint WIDTH = 800, HEIGHT = 600;
+const float toRadians = 3.14159265f / 180.0f;
 
-GLuint VAO, VBO, shader;
+GLuint VAO, VBO, shader, uniformModel;
+
+bool direction = true;
+float triOffset = 0.0f;
+float triMaxOffset = 0.7f;
+float triIncrement = 0.01f;
+
+float curAngle = 0.0f;
+
+bool sizeDirection = true;
+float curSize = 0.4f;
+float maxSize = 0.8f;
+float minSize = 0.1f;
 
 // Vertex shader
 static const char* vShader = "                                  \n\
@@ -14,20 +33,22 @@ static const char* vShader = "                                  \n\
                                                                 \n\
 layout (location = 0) in vec3 pos;                              \n\
                                                                 \n\
+uniform mat4 model;                                             \n\
+                                                                \n\
 void main()                                                     \n\
 {                                                               \n\
-    gl_Position = vec4(0.4 * pos.x, 0.4 * pos.y, pos.z, 1.0);   \n\
+    gl_Position = model * vec4(pos, 1.0);   \n\
 }";
 
 // Fragment shader
 static const char* fShader = "    \n\
 #version 330                      \n\
                                   \n\
-out vec3 color;                   \n\
+out vec4 color;                   \n\
                                   \n\
 void main()                       \n\
 {                                 \n\
-    color = vec3(1.0, 0.0, 0.0);  \n\
+    color = vec4(1.0, 0.0, 0.0, 1.0);  \n\
 }";
 
 void CreateTriangle()
@@ -112,6 +133,9 @@ void CompileShaders()
         printf("Error validating program: '%s'\n", eLog);
         return;
     }
+
+    uniformModel = glGetUniformLocation(shader, "model");
+
 }
 
 int main()
@@ -132,7 +156,7 @@ int main()
     // Allow forward compatibility
     glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
 
-    GLFWwindow* mainWindow = glfwCreateWindow(WIDTH, HEIGHT, "Test Window", NULL, NULL);
+    GLFWwindow* mainWindow = glfwCreateWindow(WIDTH, HEIGHT, "Udemy Course", NULL, NULL);
     if (!mainWindow)
     {
         printf("GLFW Window creation failed!");
@@ -162,10 +186,51 @@ int main()
     {
         glfwPollEvents();
 
+        if (direction)
+        {
+            triOffset += triIncrement;
+        }
+        else
+        {
+            triOffset -= triIncrement;
+        }
+
+        if (abs(triOffset) >= triMaxOffset)
+        {
+            direction = !direction;
+        }
+
+        curAngle += 0.5f;
+        if (curAngle >= 360)
+        {
+            curAngle -= 360;
+        }
+
+        if (sizeDirection)
+        {
+            curSize += 0.001f;
+        }
+        else
+        {
+            curSize -= 0.001f;
+        }
+
+        if (curSize >= maxSize || curSize <= minSize)
+        {
+            sizeDirection = !sizeDirection;
+        }
+
         glClearColor(0.0f, 0.0f, 1.0f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
 
         glUseProgram(shader);
+
+        glm::mat4 model(1.0f);
+        model = glm::translate(model, glm::vec3(triOffset, 0.0f, 0.0f));
+        model = glm::rotate(model, curAngle * toRadians, glm::vec3(0.0f, 0.0f, 1.0f));
+        model = glm::scale(model, glm::vec3(curSize, curSize, 1.0f));
+
+        glUniformMatrix4fv(uniformModel, 1, GL_FALSE, glm::value_ptr(model));
 
         glBindVertexArray(VAO);
         glDrawArrays(GL_TRIANGLES, 0, 3);
